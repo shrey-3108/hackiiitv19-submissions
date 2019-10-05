3f6577bf7f896a7078d5c03a916524ad535ca6c3
Shreyansh <shreyanshyadav3108@gmail.com>
2019-10-05 20:59:20 +0530

    First Commit

---
+++ team#21-{Healthcare & Biomedical Devices}/Healthcare/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Registration","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Registration","l":"Registration()","url":"%3Cinit%3E()"}]
